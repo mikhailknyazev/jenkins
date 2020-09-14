1d855d1b6bc855879a190c99099f585382ba6115
mikhailknyazev <michael.knyazev@gmail.com>
2020-09-14 22:37:37 +1000

    initial commit

---
+++ src/org/example/Constants.groovy
@@ -0,0 +1,10 @@
+package org.example
+
+/**
+ * Created by MKN.
+ * Date: 14/09/2020
+ */
+class Constants {
+    static final String SLACK_MESSAGE = "Sending Slack Notification"
+    static final String EMAIL_MESSAGE = "Sending Email"
+}
